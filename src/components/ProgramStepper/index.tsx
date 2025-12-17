ad37115952acb20f5e8ed61d0c3a6f65cab35650
deenium <nimbekar.darr@gmail.com>
2025-12-18 00:39:19 +0530

    Memoized ProgramStepper Component

--- src/components/ProgramStepper/index.tsx
+++ src/components/ProgramStepper/index.tsx
@@ -1,38 +1,43 @@
 import { StepCard, type StepCardProps } from "./StepCard";
 import "./ProgramStepper.css";
+import React from "react";
 
 interface ProgramStepperProps {
   step: number;
 }
 
-export const ProgramStepper: React.FC<ProgramStepperProps> = ({ step }) => {
-  const obtainState = (currentStep: number): "done" | "active" | "pending" => {
-    if (currentStep < step) {
-      return "done";
-    } else if (currentStep === step) {
-      return "active";
-    } else {
-      return "pending";
-    }
-  };
+export const ProgramStepper: React.FC<ProgramStepperProps> = React.memo(
+  ({ step }) => {
+    const obtainState = (
+      currentStep: number
+    ): "done" | "active" | "pending" => {
+      if (currentStep < step) {
+        return "done";
+      } else if (currentStep === step) {
+        return "active";
+      } else {
+        return "pending";
+      }
+    };
 
-  const stepCards: StepCardProps[] = [
-    { state: obtainState(1), label: "Group name", step: 1 },
-    { state: obtainState(2), label: "Structures", step: 2 },
-    { state: obtainState(3), label: "Entities", step: 3 },
-    { state: obtainState(4), label: "Members", step: 4 },
-  ];
+    const stepCards: StepCardProps[] = [
+      { state: obtainState(1), label: "Group name", step: 1 },
+      { state: obtainState(2), label: "Structures", step: 2 },
+      { state: obtainState(3), label: "Entities", step: 3 },
+      { state: obtainState(4), label: "Members", step: 4 },
+    ];
 
-  return (
-    <div className="programStepper">
-      {stepCards.map((card, index) => (
-        <StepCard
-          key={index}
-          state={card.state}
-          label={card.label}
-          step={card.step}
-        />
-      ))}
-    </div>
-  );
-};
+    return (
+      <div className="programStepper">
+        {stepCards.map((card, index) => (
+          <StepCard
+            key={index}
+            state={card.state}
+            label={card.label}
+            step={card.step}
+          />
+        ))}
+      </div>
+    );
+  }
+);
